2cf89ff30e3c0bd5d16845eb6493349613bf19a9
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-14 17:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 updateObjectWithKeyAndValue(taplist, 'Pilsner', 'Wayfinder')
 console.log(taplist)
 
+
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, {[key] : value})
 }
